94be9f45aab8f90f1a1d3226d61f63fde01f91cf
kim-jongin <whddlskss@gmail.com>
2022-12-25 15:27:05 +0900

    study: Synchronous , Asynchronous callback

---
+++ async/callback.js
@@ -0,0 +1,19 @@
+//JS는 동기적임 -> 호이스팅 이후로 코드 작성 순서에 따라 동기적으로 동작 
+//hositing: var, 함수 선언 등 선언들이 가장 상단으로 올라감 
+
+console.log('1');
+setTimeout(() => console.log('2'),1000);
+console.log('3');
+
+
+//콜백의 종류 두가지
+//1. Synchronous callback(동기적 콜백)
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log('hello'));
+//2. Asynchronous callback(비동기적 콜백)
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'));
