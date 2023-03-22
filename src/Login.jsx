c4c97a7c01d4a1e32ef8657c1a7fcb9e098ec93b
kim-jongin <whddlskss@gmail.com>
2023-03-22 18:55:00 +0900

    [feat] email validation

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    /* 정규 표현식 */
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
   return (
     <div className='page'>
       <div className='titleWrap'>
@@ -12,15 +31,22 @@ export default function Login() {
       <div className='contentWrap'>
         <div className='inputTitle'>이메일 주소</div>
         <div className='inputWrap'>
-          <input className='input' placeholder='test@test.com' />
+          <input className='input' placeholder='test@test.com' value={email} onChange={handleEmail} />
         </div>
-        <div className='errorMessageWrap'>올바른 이메일을 입력해주세요.</div>
+        <div className='errorMessageWrap'>
+          {!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}
+        </div>
 
         <div style={{ marginTop: '26px' }} className='inputTitle'>
           비밀번호
         </div>
         <div className='inputWrap'>
-          <input className='input' placeholder='영문, 숫자, 특수문자 포함 8자 이상' />
+          <input
+            className='input'
+            placeholder='영문, 숫자, 특수문자 포함 8자 이상'
+            value={pw}
+            onChange={(e) => setPw(e.target.value)}
+          />
         </div>
         <div className='errorMessageWrap'>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
       </div>
